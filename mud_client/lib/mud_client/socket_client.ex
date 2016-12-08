08c6d7a67b97256b7d8f5a3c2bce58046769d071
jacobpadkins <jpadkins@smu.edu>
2016-12-08 15:05:07 -0600

    fixing up client message loop

--- mud_client/lib/mud_client/socket_client.ex
+++ mud_client/lib/mud_client/socket_client.ex
@@ -1,98 +1,122 @@
 defmodule MudClient.SocketClient do
-  @moduledoc false
-  require Logger
   alias Phoenix.Channels.GenSocketClient
   @behaviour GenSocketClient
 
-  #######
-  # API #
-  #######
-
   ##################
-  # IMPLEMENTATION #
+  # Initialization #
   ##################
 
   def start_link() do
     GenSocketClient.start_link(
-          __MODULE__,
-          Phoenix.Channels.GenSocketClient.Transport.WebSocketClient,
-          "ws://localhost:4000/socket/websocket"
-        )
+      __MODULE__,
+      Phoenix.Channels.GenSocketClient.Transport.WebSocketClient,
+      "ws://localhost:4000/socket/websocket"
+    )
   end
 
   def init(url) do
-    {:connect, url, %{first_join: true, ping_ref: 1}}
+    {:connect, url, %{}}
   end
 
+  #def message_loop do
+  #  message = IO.gets "command> "
+  #  case String.trim(message) do
+  #    "quit" -> {:quit}
+  #    message when message != "" ->
+  #      send self, {:send_message, message}
+  #  end
+  #  message_loop
+  #end
+
+  ########################
+  # Connection Callbacks #
+  ########################
+
   def handle_connected(transport, state) do
-    Logger.info("Connected to The Server!")
+    IO.puts "Connected to The Server!"
     GenSocketClient.join(transport, "room:login")
     {:ok, state}
   end
 
   def handle_disconnected(reason, state) do
-    Logger.error("disconnected: #{inspect reason}")
-    Process.send_after(self(), :connect, :timer.seconds(1))
+    IO.puts "disconnected: #{inspect reason}"
     {:ok, state}
   end
 
-  def handle_joined(topic, _payload, _transport, state) do
-    if state.first_join do
-      :timer.send_interval(:timer.seconds(1), self(), :heartbeat)
-    end
+  def handle_joined(topic, _payload, transport, state) do
+    :timer.send_interval(:timer.seconds(10), self, :ping_server)
+    send self, {:get_message}
     {:ok, state}
   end
 
   def handle_join_error(topic, payload, _transport, state) do
-    Logger.error("join error on the topic #{topic}: #{inspect payload}")
+    IO.puts "join error on the topic #{topic}: #{inspect payload}"
     {:ok, state}
   end
 
   def handle_channel_closed(topic, payload, _transport, state) do
-    Logger.error("disconnected from the topic #{topic}: #{inspect payload}")
-    Process.send_after(self(), {:join, topic}, :timer.seconds(1))
+    IO.puts "disconnected from the topic #{topic}: #{inspect payload}"
     {:ok, state}
   end
+
+  ####################
+  # Message Handlers #
+  ####################
 
   def handle_message(topic, event, payload, _transport, state) do
-    IO.puts "#{topic}:#{event} :: #{inspect payload}"
-    {:ok, state}
-  end
-
-  def handle_reply("ping", _ref, %{"status" => "ok"} = payload, _transport, state) do
+    case event do
+      "presence_diff" -> IO.puts "presence diff"
+      "presence_state" -> IO.puts "presence state"
+      "message:user" ->
+        IO.puts Enum.join([payload["username"],
+                           " says \"",
+                           payload["body"],
+                           "\""])
+    end
     {:ok, state}
   end
 
   def handle_reply(topic, _ref, payload, _transport, state) do
-    IO.puts "Reply-#{topic} :: #{inspect payload}"
+    IO.puts "Reply-#{topic}: #{inspect payload}"
     {:ok, state}
-  end
-
-  def handle_info(:connect, _transport, state) do
-    Logger.info("connecting...")
-    {:connect, state}
   end
 
   def handle_info({:join, topic}, transport, state) do
     IO.puts "joining the topic #{topic}"
     case GenSocketClient.join(transport, topic) do
       {:error, reason} ->
-        Logger.error("error joining the topic #{topic}: #{inspect reason}")
-        Process.send_after(self(), {:join, topic}, :timer.seconds(1))
+        IO.puts "error joining the topic #{topic}: #{inspect reason}"
       {:ok, _ref} -> :ok
     end
-
     {:ok, state}
   end
 
-  def handle_info(:heartbeat, transport, state) do
-    GenSocketClient.push(transport, "room:login", "message:ping", nil)
+  def handle_info({:get_message}, _transport, state) do
+    message = Task.async(fn -> IO.gets "command> " end)
+    |> Task.await(:infinity)
+    |> String.trim
+
+    if message != "" do
+      send self, {:send_message, message}
+    end
+
+    send self, {:get_message}
+    {:ok, state}
+  end
+
+  def handle_info({:send_message, message}, transport, state) do
+    GenSocketClient.push(transport, "room:login", "message:new", message)
+    {:ok, state}
+  end
+
+  def handle_info(:ping_server, transport, state) do
+    IO.puts "sending stay-alive ping..."
+    GenSocketClient.push(transport, "room:login", "message:ping", "ping")
     {:ok, state}
   end
 
   def handle_info(message, _transport, state) do
-    Logger.warn("Unhandled message #{inspect message}")
+    IO.puts "Unhandled message #{inspect message}"
     {:ok, state}
   end
-
 end
